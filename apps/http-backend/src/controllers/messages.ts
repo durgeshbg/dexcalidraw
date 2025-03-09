350e54a50732a9fff6e6f823b7944e5532cadba9
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-03-09 08:43:18 +0530

    update: add messages controllers

---
+++ apps/http-backend/src/controllers/messages.ts
@@ -0,0 +1,45 @@
+import { prisma } from '@dexcalidraw/database';
+import { Request, Response } from 'express';
+import z from 'zod';
+
+export const roomSchema = z.object({
+  content: z.string(),
+});
+
+export const createMessage = async (req: Request, res: Response) => {
+  const { roomId } = req.params;
+  const { success, error, data } = roomSchema.safeParse(req.body);
+  if (!success) {
+    res.status(400).json({ error });
+    return;
+  }
+  const room = await prisma.room.findUnique({
+    where: { id: roomId, users: { some: { id: req.user?.id } } },
+    select: { users: true, id: true },
+  });
+  if (!room) {
+    res.status(404).json({ error: 'Room not found' });
+    return;
+  }
+  const { content } = data;
+  const message = await prisma.message.create({ data: { content, authorId: req.user?.id!, roomId: room.id } });
+  res.status(201).json({ message });
+};
+
+export const getAllMessages = async (req: Request, res: Response) => {
+  const { roomId } = req.params;
+  const room = await prisma.room.findUnique({
+    where: { id: roomId, users: { some: { id: req.user?.id! } } },
+  });
+  if (!room) {
+    res.status(404).json({ error: 'Room not found' });
+    return;
+  }
+  const messages = await prisma.message.findMany({
+    where: { roomId },
+    select: { id: true, content: true, createdAt: true, author: { select: { id: true, name: true } } },
+    orderBy: { createdAt: 'asc' },
+  });
+  res.status(200).json({ messages });
+  return;
+};
--- apps/http-backend/src/routes/rooms.ts
+++ apps/http-backend/src/routes/rooms.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middlewares/auth';
 import { createRoom, getUserRooms, deleteRoom, getRoom, addUserToRoom, removeUserFromRoom } from '../controllers/rooms';
+import { getAllMessages, createMessage } from '../controllers/messages';
 
 const router: Router = Router();
 
@@ -9,6 +10,11 @@ router.get('/', getUserRooms);
 router.post('/', createRoom);
 router.get('/:roomId', getRoom);
 router.delete('/:roomId', deleteRoom);
+// Users
 router.post('/:roomId/users', addUserToRoom);
 router.delete('/:roomId/users', removeUserFromRoom);
+// Messages
+router.get('/:roomId/messages', getAllMessages);
+router.post('/:roomId/messages', createMessage);
+
 export default router;
